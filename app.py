2e9f98e8a6dd3d344004c613b37f691db328499d
fabiogeraci <fabio.geraci@gmail.com>
2020-11-04 14:34:26 +0000

    app mode

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__, template_folder='templates')
 def init():
    global model,graph
    model = load_model('model/malaria_detector.pkl')
-   graph = tf.get_default_graph()
+#   graph = tf.get_default_graph()
 
 if __name__ == '__main__':
    print(("* Loading Keras model and Flask starting server..."
@@ -28,8 +28,8 @@ def upload_image_file():
       img = img.resize((28,28))
       im2arr = np.array(img)
       im2arr = im2arr.reshape(1,28,28,1)
-      with graph.as_default():
-         y_pred = model.predict_classes(im2arr)
+ #     with graph.as_default():
+      y_pred = model.predict_classes(im2arr)
 
       return 'Predicted Number: ' + str(y_pred[0])
 
