632cb078288b4897b514f1f435c130f2f6a75dd1
fabiogeraci <fabio.geraci@gmail.com>
2020-11-04 17:10:09 +0000

    req

--- app.py
+++ app.py
@@ -110,5 +110,5 @@ app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
 })
 
 if __name__ == "__main__":
-   app.debug = False
-   app.run(host='0.0.0.0', port=3000)
+   app.debug = True
+#   app.run(host='0.0.0.0', port=3000)
