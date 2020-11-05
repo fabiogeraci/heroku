53b56a4e5eacd77b9b11e16bef84115038ce9d17
fabiogeraci <fabio.geraci@gmail.com>
2020-11-05 06:18:35 +0000

    req

--- app.py
+++ app.py
@@ -1,7 +1,71 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, flash, url_for
+import urllib.request
+from werkzeug.utils import secure_filename
+import os
+
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+import numpy as np
+
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+from tensorflow.keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+
+# Model saved with Keras model.save()
+MODEL_PATH = 'malaria_detector.pkl'
+
+# Load your own trained model
+model = load_model(MODEL_PATH)
+model._make_predict_function()          # Necessary
+print('Model loaded. Start serving...')
 
 UPLOAD_FOLDER = '/uploads'
 
 app = Flask(__name__)
 app.secret_key = "secret key"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def get_prediction(filename, model):
+
+    image = load_img('uploads/' + filename, target_size=(224, 224))
+    image = img_to_array(image)
+    image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+    image = preprocess_input(image)
+    yhat = model.predict(image)
+    label = decode_predictions(yhat)
+    label = label[0][0]
+    print('%s (%.2f%%)' % (label[1], label[2] * 100))
+    return label[1], label[2] * 100
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/', methods=['POST'])
+def submit_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No file selected for uploading')
+            return redirect(request.url)
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+            get_prediction(filename)
+            label, acc = get_prediction(filename)
+            flash(label)
+            flash(acc)
+            flash(filename)
+            return redirect('/')
+
+
+if __name__ == "__main__":
+    app.run()
